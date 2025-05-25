1bcd27e79ea96e3e453770d0a7b24ca5f59e6c05
Null Panda <nullpandadev@gmail.com>
2025-05-25 23:14:25 +0100

    Level loading

--- godot-hello-world/scripts/coin.gd
+++ godot-hello-world/scripts/coin.gd
@@ -1,7 +1,16 @@
 extends Area2D
 
-@onready var general_manager: Node = %GeneralManager
+#@onready var level_manager = %LevelManager
+#@onready var level_manager = get_tree().get_root()
+
+signal coin_collected
 
 func _on_body_entered(body: Node2D) -> void:
-	general_manager.add_point()
+	#print("Level Manager ", level_manager)
+	#print("Level Manager ", level_manager, level_manager.name)
+	#print("Level Manager ", level_manager, level_manager.get_general_manager())
+	#return
+	#self.level_manager.get_general_manager().add_point()
+	print("coin_collected")
+	emit_signal("coin_collected")
 	queue_free()
--- godot-hello-world/scripts/general_manager.gd
+++ godot-hello-world/scripts/general_manager.gd
@@ -1,21 +1,51 @@
 extends Node
 
+#@onready var level: Node = %Level#.level_manager
 var paused = false
+
+#func _ready() -> void:
+	#print(level)
+	#level.load_general_manager(self)
+
 
 #---------------------
 # SCORE MANAGEMENT
 #---------------------
+@onready var pick_up_coin_sfx: AudioStreamPlayer2D = $PickUpCoin
+@onready var hud_coin_label = get_tree().get_first_node_in_group("HUD").get_node("TextureRect/Label") as Label
+@onready var level = %Level
+@onready var game = self.get_tree().current_scene as Node2D
+@onready var player = %Player
+@onready var player_start = player.position
+
 var score = 0
 
-# I kind of cheated this. Tutorial wanted me to use animation but I refused
-@onready var pick_up_coin_sfx: AudioStreamPlayer2D = $PickUpCoin
+func load_level2():
+	if $Level:
+		$Level.queue_free()
 
-@onready var hud_coin_label = get_tree().get_first_node_in_group("HUD").get_node("TextureRect/Label") as Label
+	await get_tree().process_frame  # Wait a frame to fully remove the old level
 
-func add_point():
+	var new_level = load("res://scenes/Level2.tscn").instantiate()
+	add_child(new_level)
+	player.position = player_start
+	connect_to_coins()
+
+func _ready():
+	connect_to_coins()
+
+func connect_to_coins():
+	# Connect coin signals
+	for coin in level.get_tree().get_nodes_in_group("coin"):
+		coin.connect("coin_collected", Callable(self, "_on_coin_collected"))
+
+func _on_coin_collected():
 	pick_up_coin_sfx.play()
 	score += 1
 	hud_coin_label.text = "x" + str(score)
+	
+	if score == 2:
+		load_level2()
 
 
 #---------------------
